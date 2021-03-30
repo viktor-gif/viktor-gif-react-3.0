89eaacba7dc763623ea5e580c306cc6b7d79a683
viktor-gif <klim.victor.ua@gmail.com>
2021-03-30 11:10:47 +0300

    lesson 54

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -6,6 +6,9 @@ import userAvatar from "../../img/ava.png";
 class Users extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
