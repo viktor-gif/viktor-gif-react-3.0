e23319614d9baf4951f65728f108efcc203262ab
viktor-gif <klim.victor.ua@gmail.com>
2021-03-30 10:50:22 +0300

    lesson 53

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -3,49 +3,46 @@ import React from "react";
 import s from "./users.module.css";
 import userAvatar from "../../img/ava.png";
 
-const Users = (props) => {
-  const getUsers = () => {
-    if (props.users.length === 0) {
-      axios
-        .get("https://social-network.samuraijs.com/api/1.0/users")
-        .then((response) => {
-          props.getUsers(response.data.items);
-        });
-    }
-  };
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        this.props.getUsers(response.data.items);
+      });
+  }
 
-  const usersItems = props.users.map((u) => {
-    return (
-      <div key={u.id} className={s.user}>
-        <div className={s.userPhoto}>
-          <img src={u.photos.small ?? userAvatar} alt="photo" />
+  render() {
+    const usersItems = this.props.users.map((u) => {
+      return (
+        <div key={u.id} className={s.user}>
+          <div className={s.userPhoto}>
+            <img src={u.photos.small ?? userAvatar} alt="photo" />
+          </div>
+          <div>
+            {u.followed ? (
+              <button onClick={() => this.props.unfollow(u.id)}>
+                Unfollow
+              </button>
+            ) : (
+              <button onClick={() => this.props.follow(u.id)}>Follow</button>
+            )}
+          </div>
+          <div>
+            <b>Name</b> :{u.name}
+          </div>
+          <div>
+            <b>Status</b> :{u.status ?? "--------"}
+          </div>
+          <div>u.country</div>
+          <div>u.city</div>
         </div>
-        <div>
-          {u.followed ? (
-            <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-          ) : (
-            <button onClick={() => props.follow(u.id)}>Follow</button>
-          )}
-        </div>
-        <div>
-          <b>Name</b> :{u.name}
-        </div>
-        <div>
-          <b>Status</b> :{u.status ?? "--------"}
-        </div>
-        <div>u.country</div>
-        <div>u.city</div>
-      </div>
-    );
-  });
-
-  return (
-    <div>
-      <button onClick={getUsers}>Get users</button>
-      {usersItems}
-    </div>
-  );
-};
+      );
+    });
+    return <div>{usersItems}</div>;
+  }
+}
 
 // const User = (props) => {
 //   const follow = () => {
