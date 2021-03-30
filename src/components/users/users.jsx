ad4ad842b0e50a9674e39d5ca83387b4e8ab369a
viktor-gif <klim.victor.ua@gmail.com>
2021-03-30 10:43:37 +0300

    lesson 42

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -1,44 +1,24 @@
+import axios from "axios";
 import React from "react";
 import s from "./users.module.css";
+import userAvatar from "../../img/ava.png";
 
 const Users = (props) => {
-  if (props.users.length === 0) {
-    props.getUsers([
-      {
-        id: 1,
-        imgUrl:
-          "https://wikicomics.ru/uploads/posts/2015-09/1442533395_doktor-dum-v-filme-fantasticheskaya-chetverka-2005-goda.jpg",
-        followed: false,
-        name: "Viktor",
-        status: "Hello",
-        location: { country: "Ukraine", city: "Kyev" },
-      },
-      {
-        id: 2,
-        imgUrl:
-          "https://wikicomics.ru/uploads/posts/2015-09/1442533395_doktor-dum-v-filme-fantasticheskaya-chetverka-2005-goda.jpg",
-        followed: true,
-        name: "Dima",
-        status: "I'm a cool developer",
-        location: { country: "Belarus", city: "Minsk" },
-      },
-      {
-        id: 3,
-        imgUrl:
-          "https://wikicomics.ru/uploads/posts/2015-09/1442533395_doktor-dum-v-filme-fantasticheskaya-chetverka-2005-goda.jpg",
-        followed: false,
-        name: "Vova",
-        status: "Hello world!!!!",
-        location: { country: "Russia", city: "Moskov" },
-      },
-    ]);
-  }
+  const getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          props.getUsers(response.data.items);
+        });
+    }
+  };
 
   const usersItems = props.users.map((u) => {
     return (
       <div key={u.id} className={s.user}>
         <div className={s.userPhoto}>
-          <img src={u.imgUrl} alt="photo" />
+          <img src={u.photos.small ?? userAvatar} alt="photo" />
         </div>
         <div>
           {u.followed ? (
@@ -47,15 +27,24 @@ const Users = (props) => {
             <button onClick={() => props.follow(u.id)}>Follow</button>
           )}
         </div>
-        <div>{u.name}</div>
-        <div>{u.status}</div>
-        <div>{u.country}</div>
-        <div>{u.city}</div>
+        <div>
+          <b>Name</b> :{u.name}
+        </div>
+        <div>
+          <b>Status</b> :{u.status ?? "--------"}
+        </div>
+        <div>u.country</div>
+        <div>u.city</div>
       </div>
     );
   });
 
-  return <div>{usersItems}</div>;
+  return (
+    <div>
+      <button onClick={getUsers}>Get users</button>
+      {usersItems}
+    </div>
+  );
 };
 
 // const User = (props) => {
