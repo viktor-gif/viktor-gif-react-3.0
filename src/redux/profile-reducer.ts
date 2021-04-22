7d9061bb55631bc3f7a0bb3bf1bd96f1ecb018dc
viktor-gif <klim.victor.ua@gmail.com>
2021-04-22 05:09:03 +0300

    2.0 (TS): @type profile-reducer

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -8,17 +8,45 @@ const SET_STATUS = "vgif/profile/SET_STATUS";
 const LOAD_PROFILE_PHOTO = "vgif/profile/LOAD_PROFILE_PHOTO";
 const SAVE_PHOTO_SUCCESS = "vgif/profile/SAVE_PHOTO_SUCCESS";
 
+type postsType = { id: number; post: string; likesCount: number };
+type profileInfoType = {
+  userId: number;
+  lookingForAJob: boolean;
+  lookingForAJobDescription: string;
+  fullName: string;
+  aboutMe: string;
+  contacts: contactsType;
+  photos: photosType;
+};
+type contactsType = {
+  github: string | null;
+  vk: string | null;
+  facebook: string | null;
+  instagram: string | null;
+  twitter: string | null;
+  website: string | null;
+  youtube: string | null;
+  mainLink: string | null;
+};
+type photosType = {
+  small: string | null;
+  large: string | null;
+};
+type initialStateType = typeof initialState;
 let initialState = {
   posts: [
     { id: 1, post: "Hello", likesCount: 5 },
     { id: 2, post: "I'm a cool developer", likesCount: 23 },
     { id: 3, post: "Hello world!!!!", likesCount: 0 },
-  ],
-  profileInfo: null,
+  ] as Array<postsType>,
+  profileInfo: null as profileInfoType | null,
   status: "",
 };
 
-const profileReducer = (state = initialState, action) => {
+const profileReducer = (
+  state = initialState,
+  action: any
+): initialStateType => {
   switch (action.type) {
     case ADD_POST:
       let newPost = {
@@ -48,36 +76,70 @@ const profileReducer = (state = initialState, action) => {
     case SAVE_PHOTO_SUCCESS:
       return {
         ...state,
-        profileInfo: { ...state.profileInfo, photos: action.photoFile },
+        profileInfo: {
+          ...state.profileInfo,
+          photos: action.photoFile,
+        } as profileInfoType,
       };
     default:
       return state;
   }
 };
 
-export const addPost = (postText) => ({ type: ADD_POST, postText });
-export const deletePost = (id) => ({ type: DELETE_POST, id });
+type addPostType = { type: typeof ADD_POST; postText: string };
+export const addPost = (postText: any): addPostType => ({
+  type: ADD_POST,
+  postText,
+});
 
-export const setProfile = (profileInfo) => ({ type: SET_PROFILE, profileInfo });
-export const setStatus = (status) => ({ type: SET_STATUS, status });
-export const loadProfilePhoto = (file) => ({ type: LOAD_PROFILE_PHOTO, file });
-export const savePhotoSuccess = (photoFile) => ({
+type deletePostType = { type: typeof DELETE_POST; id: number };
+export const deletePost = (id: number): deletePostType => ({
+  type: DELETE_POST,
+  id,
+});
+
+type setProfileType = {
+  type: typeof SET_PROFILE;
+  profileInfo: profileInfoType;
+};
+export const setProfile = (profileInfo: profileInfoType): setProfileType => ({
+  type: SET_PROFILE,
+  profileInfo,
+});
+
+type setStatusType = { type: typeof SET_STATUS; status: string };
+export const setStatus = (status: string): setStatusType => ({
+  type: SET_STATUS,
+  status,
+});
+
+type loadProfilePhotoType = { type: typeof LOAD_PROFILE_PHOTO; file: any };
+export const loadProfilePhoto = (file: any): loadProfilePhotoType => ({
+  type: LOAD_PROFILE_PHOTO,
+  file,
+});
+
+type savePhotoSuccessType = {
+  type: typeof SAVE_PHOTO_SUCCESS;
+  photoFile: any;
+};
+export const savePhotoSuccess = (photoFile: any): savePhotoSuccessType => ({
   type: SAVE_PHOTO_SUCCESS,
   photoFile,
 });
 
 //redux-thunk
-export const getProfile = (userId) => (dispatch) => {
+export const getProfile = (userId: number) => (dispatch: any) => {
   profileAPI.setProfile(userId).then((response) => {
     dispatch(setProfile(response.data));
   });
 };
-export const getStatus = (userId) => (dispatch) => {
+export const getStatus = (userId: number) => (dispatch: any) => {
   profileAPI.getStatus(userId).then((response) => {
     dispatch(setStatus(response.data));
   });
 };
-export const updateStatus = (status) => async (dispatch) => {
+export const updateStatus = (status: string) => async (dispatch: any) => {
   try {
     let response = await profileAPI.updateStatus(status);
     if (response.data.resultCode === 0) {
@@ -87,14 +149,19 @@ export const updateStatus = (status) => async (dispatch) => {
     console.log(error);
   }
 };
-export const setProfilePhoto = (file) => (dispatch) => {
+export const setProfilePhoto = (file: any) => (dispatch: any) => {
   profileAPI.setProfilePhoto(file).then((response) => {
     if (response.data.resultCode === 0) {
       dispatch(savePhotoSuccess(response.data.data.photos));
     }
   });
 };
-export const updateProfileInfo = (info) => async (dispatch, getState) => {
+
+// thunk-creators
+export const updateProfileInfo = (info: any) => async (
+  dispatch: any,
+  getState: any
+) => {
   const userId = getState().auth.userId;
   let response = await profileAPI.updateProfileInfo(info);
   if (response.data.resultCode === 0) {
