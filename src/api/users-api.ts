7ae37ae5cab9263ecf547c56b9950e4e933dcd78
viktor-gif <klim.victor.ua@gmail.com>
2021-06-14 20:36:55 +0300

    2.0 (TS): auth-api

--- src/api/users-api.ts
+++ src/api/users-api.ts
@@ -1,16 +1,12 @@
-import { instance, ResultCodesEnum } from "./api";
+import { instance, responseType, ResultCodesEnum } from "./api";
 import { userType } from "../Types";
+import { AxiosPromise } from "axios";
 
 // authAPI.getAuthData().then((res: AxiosResponse<any>) => res.data);
 type getUsersResType = {
   items: Array<userType>;
   totalCount: number;
   error: string | null;
-};
-type followUnfollowResType = {
-  resultCode: ResultCodesEnum;
-  messages: Array<string>;
-  data: Object;
 };
 export const usersAPI = {
   getUsers(currentPage = 1, pageSize = 10) {
@@ -21,12 +17,12 @@ export const usersAPI = {
 
   follow(userId: number) {
     return instance
-      .post<followUnfollowResType>(`follow/${userId}`)
+      .post<responseType>(`follow/${userId}`)
       .then((res) => res.data);
   },
   unfollow(userId: number) {
     return instance
-      .delete<followUnfollowResType>(`follow/${userId}`)
-      .then((res) => res.data);
+      .delete(`follow/${userId}`)
+      .then((res) => res.data) as Promise<responseType>;
   },
 };
