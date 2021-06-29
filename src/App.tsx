5ee9cfca33c2e2fc29f5f296fa18416e75ebe37d
viktor-gif <klim.victor.ua@gmail.com>
2021-06-29 16:42:14 +0300

    2.0 (lesson 17): Ant Design 1

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,20 @@ import { getAuthData } from "./redux/auth-reducer";
 import { initialize } from "./redux/app-reducer";
 import { connect } from "react-redux";
 import { compose } from "redux";
+import "antd/dist/antd.css";
+import { Button } from "antd";
+
+import { Layout, Menu } from "antd";
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+
+const { Header, Sider, Content } = Layout;
+
 const DialogsContainer = React.lazy(
   () => import("./components/dialogs/dialogsContainer")
 );
@@ -23,7 +37,26 @@ type dispatchPropsType = {
   getAuthData: () => void;
 };
 
+// class SiderDemo extends React.Component {
+//   state = {
+//     collapsed: false,
+//   };
+
+//   toggle = () => {
+//     this.setState({
+//       collapsed: !this.state.collapsed,
+//     });
+//   };
+
 class App extends React.Component<mapPropsType & dispatchPropsType> {
+  state = {
+    collapsed: false,
+  };
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
   catchAllUnhandledErrors = (promiseRejectionEvent: PromiseRejectionEvent) => {
     alert(promiseRejectionEvent.reason);
   };
@@ -43,45 +76,129 @@ class App extends React.Component<mapPropsType & dispatchPropsType> {
     if (!this.props.initialized) {
       return <h1>Loading...</h1>;
     }
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <HeaderContainer />
-          <Navbar />
-          <div className="content-wrapper">
-            <Switch>
-              {/* <Route exact path="/" render={() => <Redirect to="/profile" />} /> */}
-              <Route
-                path="/profile/:userId?"
-                render={() => {
-                  return (
-                    <React.Suspense fallback={<div>Loading...</div>}>
-                      <Profile />
-                    </React.Suspense>
-                  );
-                }}
-              />
-              <Route
-                path="/dialogs"
-                render={() => {
-                  return (
-                    <React.Suspense fallback={<div>Loading...</div>}>
-                      <DialogsContainer />
-                    </React.Suspense>
-                  );
-                }}
-              />
-              <Route path="/users" render={() => <UsersPage />} />
-              <Route path="/login/facebook" render={() => <h1>Facebook</h1>} />
-              <Route path="/login" render={() => <LoginPage />} />
-              <Redirect from="/" to="/profile" />
-              <Route path="*" render={() => <div>404 NOT FOUND!</div>} />
-            </Switch>
-          </div>
 
-          <Footer />
-        </div>
-      </BrowserRouter>
+    return (
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              nav 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(
+              this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+              {
+                className: "trigger",
+                onClick: this.toggle,
+              }
+            )}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            <BrowserRouter>
+              <div className="App">
+                {/* <HeaderContainer />
+                <Navbar /> */}
+                <div className="content-wrapper">
+                  <Switch>
+                    <Route
+                      exact
+                      path="/"
+                      render={() => <Redirect to="/profile" />}
+                    />
+                    <Route
+                      path="/profile/:userId?"
+                      render={() => {
+                        return (
+                          <React.Suspense fallback={<div>Loading...</div>}>
+                            <Profile />
+                          </React.Suspense>
+                        );
+                      }}
+                    />
+                    <Route
+                      path="/dialogs"
+                      render={() => {
+                        return (
+                          <React.Suspense fallback={<div>Loading...</div>}>
+                            <DialogsContainer />
+                          </React.Suspense>
+                        );
+                      }}
+                    />
+                    <Route path="/users" render={() => <UsersPage />} />
+                    <Route
+                      path="/login/facebook"
+                      render={() => <h1>Facebook</h1>}
+                    />
+                    <Route path="/login" render={() => <LoginPage />} />
+                    <Redirect from="/" to="/profile" />
+                    <Route path="*" render={() => <div>404 NOT FOUND!</div>} />
+                  </Switch>
+                </div>
+
+                {/* <Footer /> */}
+              </div>
+            </BrowserRouter>
+          </Content>
+        </Layout>
+      </Layout>
+
+      // <BrowserRouter>
+      //   <div className="App">
+      //     <HeaderContainer />
+      //     <Navbar />
+      //     <div className="content-wrapper">
+      //       <Switch>
+      //         {/* <Route exact path="/" render={() => <Redirect to="/profile" />} /> */}
+      //         <Route
+      //           path="/profile/:userId?"
+      //           render={() => {
+      //             return (
+      //               <React.Suspense fallback={<div>Loading...</div>}>
+      //                 <Button type="primary">Ant Design</Button>
+      //                 <Profile />
+      //               </React.Suspense>
+      //             );
+      //           }}
+      //         />
+      //         <Route
+      //           path="/dialogs"
+      //           render={() => {
+      //             return (
+      //               <React.Suspense fallback={<div>Loading...</div>}>
+      //                 <DialogsContainer />
+      //               </React.Suspense>
+      //             );
+      //           }}
+      //         />
+      //         <Route path="/users" render={() => <UsersPage />} />
+      //         <Route path="/login/facebook" render={() => <h1>Facebook</h1>} />
+      //         <Route path="/login" render={() => <LoginPage />} />
+      //         <Redirect from="/" to="/profile" />
+      //         <Route path="*" render={() => <div>404 NOT FOUND!</div>} />
+      //       </Switch>
+      //     </div>
+
+      //     <Footer />
+      //   </div>
+      // </BrowserRouter>
     );
   }
 }
