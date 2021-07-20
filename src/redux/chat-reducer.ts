c6a55a50843d6450b532abfb1d2b34c46137966a
viktor-gif <klim.victor.ua@gmail.com>
2021-07-20 16:10:33 +0300

    2.0 (lesson 20): WebSocket + React + Redux (added chat-reducer)

---
+++ src/redux/chat-reducer.ts
@@ -0,0 +1,67 @@
+import { Dispatch } from "redux";
+import { stopSubmit } from "redux-form";
+import { ThunkAction } from "redux-thunk";
+import { appStateType, inferActionsTypes } from "./redux-store";
+import { chatMessageType, chatAPI } from "../api/chat-api";
+
+let initialState = {
+  messages: [] as chatMessageType[],
+  login: null as string | null,
+  email: null as string | null,
+  isAuth: false,
+  captchaUrl: null as string | null,
+};
+
+const chatReducer = (
+  state = initialState,
+  action: actionsTypes
+): initialStateType => {
+  switch (action.type) {
+    case "vgif/chat/MESSAGES_RECEIVED":
+      return {
+        ...state,
+        messages: [...state.messages, ...action.payload.messages],
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const actions = {
+  messagesReceived: (messages: chatMessageType[]) =>
+    ({
+      type: "vgif/chat/MESSAGES_RECEIVED",
+      payload: { messages },
+    } as const),
+};
+
+let _newMessagehandler: ((messages: chatMessageType[]) => void) | null = null;
+const newMessagehandlerCreator = (dispatch: dispatchType) => {
+  if (_newMessagehandler === null) {
+    _newMessagehandler = (messages) => {
+      dispatch(actions.messagesReceived(messages));
+    };
+  }
+  return _newMessagehandler;
+};
+//redux-thunks
+export const startMessagesListening = () => (dispatch: dispatchType) => {
+  chatAPI.subscribe(newMessagehandlerCreator(dispatch));
+};
+export const stopMessagesListening = () => (dispatch: dispatchType) => {
+  chatAPI.unsubscribe(newMessagehandlerCreator(dispatch));
+};
+
+export default chatReducer;
+
+type initialStateType = typeof initialState;
+type actionsTypes = inferActionsTypes<typeof actions>;
+
+type dispatchType = Dispatch<actionsTypes>;
+type thunkType = ThunkAction<
+  void,
+  appStateType,
+  unknown,
+  actionsTypes | ReturnType<typeof stopSubmit>
+>;
